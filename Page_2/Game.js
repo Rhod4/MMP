22f880cbc105dba28cf41f24453055b3675637d6
Rhod4 <Rhod4@users.noreply.github.com>
2020-02-27 17:46:09 +0000

    storing squares as object and clearing the canvas

--- Page_2/Game.js
+++ Page_2/Game.js
@@ -3,19 +3,42 @@
 var c = document.getElementById("myCanvas");
 var ctx = c.getContext("2d");
 
+var a = 10;
+
+let rects = {
+  redSquare: { x: 10, y: 10, w: 10, h: 10, fillStyle: "Red"},
+  blueSquare: { x: 100, y: 10, w: 10, h: 10,fillStyle: "Blue"}
+};
+
+Background();
+DrawRects();
+
+function Background(){
+  ctx.beginPath();
+  ctx.rect(0, 0,myCanvas.width, myCanvas.height, );
+  ctx.fillStyle = "gray";
+  ctx.fill();
+}
+
+function DrawRects(){
+for (var rect in rects) {
+    ctx.beginPath();
+    ctx.rect(rects[rect].x, rects[rect].y, rects[rect].w, rects[rect].h);
+    ctx.fillStyle = rects[rect].fillStyle;
+    ctx.fill();
+  };
+}
+
+function ReDrawRects(){
+  ctx.clearRect(0,0, myCanvas.width, myCanvas.height);
+  Background();
+  DrawRects();
+}
 
 
+document.getElementById("demo").onmousedown = function() {
+  rects["redSquare"].x += 10;
+  ReDrawRects();
+  document.getElementById("demo").innerHTML = rects.size;
 
-ctx.beginPath();
-ctx.rect(0, 0,myCanvas.width, myCanvas.height, );
-ctx.fillStyle = "gray";
-ctx.fill();
-
-
-ctx.beginPath();
-ctx.rect(0, 0,30,30, );
-ctx.fillStyle = "blue";
-ctx.fill();
-
-
-document.getElementById("demo").onmousedown = function() {window.location = "https://www.google.com";};
+}
