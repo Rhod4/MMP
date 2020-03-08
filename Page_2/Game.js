83928b03276e5e82d64c4282725ac99475842f9f
Rhod4 <Rhod4@users.noreply.github.com>
2020-03-08 01:13:27 +0000

    Onclick, Correct numbers in div, no duplicate answers, tidying func code

--- Page_2/Game.js
+++ Page_2/Game.js
@@ -1,92 +1,109 @@
-
-
-
-  let rects = {
-    redSquare: { x: 10, y: 10, w: 50, h: 50, fillStyle: "red", set: 1},
-    blueSquare: { x: 95, y: 10, w: 50, h: 50,fillStyle: "lightblue",set: 2},
-      yellowSquare: { x: 190, y: 10, w: 50, h: 50,fillStyle: "yellow",set: 3},
-      PinkSquare: { x: 10, y: 90, w: 50, h: 50, fillStyle: "pink",set: 4},
-      greenSquare: { x: 95, y: 90, w: 50, h: 50,fillStyle: "lightgreen",set: 5},
-        whiteSquare: { x: 190, y: 90, w: 50, h: 50,fillStyle: "white",set: 6}
-  };
-  var first = 0;
-  var second = 0;
-var answer = 0;
-var squareQuestion = 0;
+let rects = {
+  redSquare: { x: 10, y: 10, w: 50, h: 50, fillStyle: "red", set: 0},
+  blueSquare: { x: 95, y: 10, w: 50, h: 50,fillStyle: "lightblue",set: 1},
+  yellowSquare: { x: 190, y: 10, w: 50, h: 50,fillStyle: "yellow",set: 2},
+  PinkSquare: { x: 10, y: 90, w: 50, h: 50, fillStyle: "pink",set: 3},
+  greenSquare: { x: 95, y: 90, w: 50, h: 50,fillStyle: "lightgreen",set: 4},
+  whiteSquare: { x: 190, y: 90, w: 50, h: 50,fillStyle: "white",set: 5}
+};
+var score = 0;
+var first;
+var second;
+var answer;
+var answerSquareFinder;
 DivMaker();
 
-  function Background(){
-    ctx.beginPath();
-    ctx.rect(0, 0,myCanvas.width, myCanvas.height, );
-    ctx.fillStyle = "gray";
-    ctx.fill();
-  }
-
-  function ReDrawRects(){
-    ctx.clearRect(0,0, myCanvas.width, myCanvas.height);
-    Background();
-    DrawRects();
-  }
-
-  function RandomQuestion(){
-    first = Math.floor(Math.random() * 11);
-    second = Math.floor(Math.random() * 11);
-for (var rect in rects) {
-      document.getElementById(rect).innerHTML = Math.floor(Math.random() * 11);
-    };
-  }
-
-  document.getElementById("demo").onmousedown = function() {
-
-    RandomQuestion();
-    answer = first + second
-
-    document.getElementById("demo").innerHTML = first + "+" + second;
+function RandomQuestion(){
   first = Math.floor(Math.random() * 11);
+  second = Math.floor(Math.random() * 11);
+  answerSquareFinder = Math.floor(Math.random() * 5);
+  console.log(answerSquareFinder);
+}
 
-  }
+
+
+document.getElementById("demo").onmousedown = function() {
+  RandomQuestion();
+  answer = first + second
+  document.getElementById("demo").innerHTML = first + "+" + second;
+  NumberInSquareChecker();
+  SameAnswerChecker();
+}
 
 
 {
   document.getElementById("redSquare").addEventListener("mousedown", function (event) {
-      console.log("redSquare");
+    AnswerChecker(sqaureName = "redSquare");
   });
   document.getElementById("blueSquare").addEventListener("mousedown", function (event) {
-      console.log("blueSquare");
+    AnswerChecker(sqaureName = "blueSquare");
   });
   document.getElementById("yellowSquare").addEventListener("mousedown", function (event) {
-      console.log("yellowSquare");
+    AnswerChecker(sqaureName = "yellowSquare");
   });
   document.getElementById("PinkSquare").addEventListener("mousedown", function (event) {
-      console.log("PinkSquare");
+    AnswerChecker(sqaureName = "PinkSquare");
   });
   document.getElementById("greenSquare").addEventListener("mousedown", function (event) {
-      console.log("greenSquare");
+    AnswerChecker(sqaureName = "greenSquare");
   });
   document.getElementById("whiteSquare").addEventListener("mousedown", function (event) {
-      console.log("whiteSquare");
+    AnswerChecker(sqaureName = "whiteSquare");
   });
+
 }
 
-  function DivMaker(){
-    //for loop that runs for every object in the rects array
-    for (var rect in rects) {
+function DivMaker(){
+  //for loop that runs for every object in the rects array
+  for (var rect in rects) {
+    //creates a div object out of the rects array
+    var newDiv = document.createElement(rect);
+    newDiv.style.width = "100px";
+    newDiv.style.height = "100px";
+    newDiv.style.background = rects[rect].fillStyle;
+    newDiv.style.margin = "10px";
+    newDiv.id = rect;
+    newDiv.style.left = rects[rect].x+'px';
+    newDiv.style.top = rects[rect].y+'px';
+    newDiv.style.aling = "center";
 
-   var newDiv = document.createElement(rect);
-   newDiv.style.width = "100px";
-   newDiv.style.height = "100px";
-  newDiv.style.background = rects[rect].fillStyle;
-  newDiv.style.margin = "10px";
-  newDiv.id = rect;
-  newDiv.style.left = rects[rect].x+'px';
-  newDiv.style.top = rects[rect].y+'px';
-newDiv.style.aling = "center";
+    var newContent = document.createTextNode("");
+    newDiv.appendChild(newContent);
 
-     var newContent = document.createTextNode("Hi there and greetings!");
-      newDiv.appendChild(newContent);
-
-        var currentDiv = document.getElementById("JavaGame");
+    var currentDiv = document.getElementById("JavaGame");
     document.body.insertBefore(newDiv, currentDiv);
     document.getElementById('JavaGame').appendChild(newDiv);
-      };
+  };
+}
+
+/*function that sets one of the answer divs to be correct*/
+function SameAnswerChecker(){
+  for (var rect in rects) {
+    if (rects[rect].set == answerSquareFinder){
+      document.getElementById(rect).innerHTML = answer;
+    }
+  };
+}
+
+
+
+function NumberInSquareChecker(){
+  for (var rect in rects) {
+    do{
+      document.getElementById(rect).innerHTML = Math.floor(Math.random() * 21);
+    }
+    while (document.getElementById(rect).innerHTML == answer)
+  };
+}
+
+function CorrectAnswer(){
+  score ++;
+  console.log(score);
+}
+
+function AnswerChecker(sqaureName){
+  if (rects[sqaureName].set == answerSquareFinder){
+    CorrectAnswer();
   }
+
+}
