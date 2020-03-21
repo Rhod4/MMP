d8f93a49a2fa26e60289207ad2384e775915b1ce
Rhod4 <Rhod4@users.noreply.github.com>
2020-03-21 11:19:09 +0000

    New DataBase connections

--- Connection/db_connection.php
+++ Connection/db_connection.php
@@ -4,7 +4,7 @@ function OpenCon()
  $dbhost = "db.dcs.aber.ac.uk";
  $dbuser = "rhs24";
  $dbpass = "rhodri18";
- $db = "cs39930_19_20_rhs24";
+ $db = "cs39440_19_20_rhs24";
  $conn = new mysqli($dbhost, $dbuser, $dbpass,$db) or die("Connect failed: %s\n". $conn -> error);
  
  return $conn;
